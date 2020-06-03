e9a6b13d43328daff75eeb7e21f56e5238da8e8a
HarshRaj0942 <f20170942@goa.bits-pilani.ac.in>
2020-06-04 00:08:18 +0530

    new campgrounds being added via a form

--- app.js
+++ app.js
@@ -2,8 +2,32 @@ var express = require("express");
 
 var app = express();
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
+
+//global object array as data for now
+var campgrounds = [
+  {
+    name: "Bihar",
+    image:
+      "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2019/04/Summer-Camping-Sites-India.jpg",
+  },
+  {
+    name: "Rajasthan",
+    image:
+      "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2019/04/Summer-Camping-Jaisalmer-Rajasthan.jpg",
+  },
+  {
+    name: "Gujarat",
+    image:
+      "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2019/04/Summer-Camping-Rann-of-Kutch-Gujarat.jpg",
+  },
+];
+
 //server listening at port 6969
+
 app.listen(6969, function () {
   console.log("The WalkerCamp Server listening at port 6969");
 });
@@ -15,22 +39,26 @@ app.get("/", function (req, res) {
 
 //campgrounds route
 app.get("/campgrounds", function (req, res) {
-  var campgrounds = [
-    {
-      name: "Bihar",
-      image:
-        "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2019/04/Summer-Camping-Sites-India.jpg",
-    },
-    {
-      name: "Rajasthan",
-      image:
-        "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2019/04/Summer-Camping-Jaisalmer-Rajasthan.jpg",
-    },
-    {
-      name: "Gujarat",
-      image:
-        "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2019/04/Summer-Camping-Rann-of-Kutch-Gujarat.jpg",
-    },
-  ];
   res.render("campgrounds.ejs", { campgrounds: campgrounds });
+});
+
+//show the form to create new camp grounds
+app.get("/campgrounds/new", function (req, res) {
+  res.render("new.ejs");
+});
+
+//creating new campgrounds ,REST convention
+app.post("/campgrounds", function (req, res) {
+  //get data from form and add to Walker camp
+
+  var name = req.body.ground_Name;
+  var image = req.body.ground_Image;
+
+  //make an object with the data obtained
+
+  var newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+
+  //redirect back to campgrounds page
+  res.redirect("/campgrounds");
 });
