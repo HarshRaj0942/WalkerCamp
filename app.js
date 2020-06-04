edb2ff194ab222f1001dd08c49bfcca3e9f09af1
HarshRaj0942 <f20170942@goa.bits-pilani.ac.in>
2020-06-04 22:45:54 +0530

    mongoose added to the project, data persistence achieved

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require("express");
+var mongoose = require("mongoose");
 
+//connect to our database
+mongoose.connect("mongodb://localhost:27017/WalkerCamp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 var app = express();
 
 var bodyParser = require("body-parser");
@@ -7,30 +13,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-//global object array as data for now
-var campgrounds = [
-  {
-    name: "Bihar",
-    image:
-      "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2019/04/Summer-Camping-Sites-India.jpg",
-  },
-  {
-    name: "Rajasthan",
-    image:
-      "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2019/04/Summer-Camping-Jaisalmer-Rajasthan.jpg",
-  },
-  {
-    name: "Gujarat",
-    image:
-      "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2019/04/Summer-Camping-Rann-of-Kutch-Gujarat.jpg",
-  },
-];
-
 //server listening at port 6969
 
 app.listen(6969, function () {
   console.log("The WalkerCamp Server listening at port 6969");
 });
+
+//schema setup
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+});
+
+//setup the campground Model
+
+var campgroundModel = mongoose.model("campground", campgroundSchema);
+
+campgroundModel.create(
+  {
+    name: "Surat",
+    image:
+      "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2019/04/Summer-Camping-Har-Ki-Dun-Uttarakhand.jpg",
+  },
+  function (err, campground) {
+    if (err) console.log("error!");
+    else console.log("NEW CAMPGROUND!!");
+    console.log(campground);
+  }
+);
+
+//add another campground
+campgroundModel.create(
+  {
+    name: "Dhanbad",
+    image:
+      "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2019/04/Summer-Camping-Jaisalmer-Rajasthan.jpg",
+  },
+  function (err, campground) {
+    if (err) console.log("error!");
+    else console.log("NEW CAMPGROUND!!");
+    console.log(campground);
+  }
+);
 
 //landing page route
 app.get("/", function (req, res) {
@@ -39,7 +64,15 @@ app.get("/", function (req, res) {
 
 //campgrounds route
 app.get("/campgrounds", function (req, res) {
-  res.render("campgrounds.ejs", { campgrounds: campgrounds });
+  //get all campground from database and render it
+
+  campgroundModel.find({}, function (err, allcampground) {
+    if (err) console.log("Error in obtaining campgrounds!");
+    else {
+      res.render("campgrounds.ejs", { campgrounds: allcampground });
+    }
+  });
+  //res.render("campgrounds.ejs", { campgrounds: campgrounds });
 });
 
 //show the form to create new camp grounds
@@ -57,8 +90,16 @@ app.post("/campgrounds", function (req, res) {
   //make an object with the data obtained
 
   var newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
+
+  //create a new campground and save to the database
+
+  campgroundModel.create(newCampground, function (err, campground) {
+    if (err) console.log("Error in creating new campgrounds!");
+    else {
+      //redirect to campgrounds page
+      res.redirect("/campgrounds");
+    }
+  });
 
   //redirect back to campgrounds page
-  res.redirect("/campgrounds");
 });
